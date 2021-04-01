64df80450735feed013f73aa1ca9972820ba633d
Tom Y <tomyeo26@gmail.com>
2021-04-01 14:17:02 +0100

    Added javascript functionality, to change style sheets and to collapse nav on scroll

---
+++ resources/javaScript/script.js
@@ -0,0 +1,30 @@
+const changeStyleSheet = (sheet) => {
+    document.getElementById("pageStyle").setAttribute("href", sheet);
+};
+
+const changeStyleColour = () => {
+    let styleColour = document.getElementById("changeColour");
+
+    styleColour.onclick = function () {
+            if(document.getElementById("changeColour").innerHTML === "Dark" ) {
+                changeStyleSheet("./resources/css/darkStyle.css")
+                document.getElementById("changeColour").innerHTML = "Light";
+            } else {
+                changeStyleSheet("./resources/css/lightStyle.css")
+                document.getElementById("changeColour").innerHTML = "Dark";
+            }
+}
+};
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navBar").style.top = "0";
+  } else {
+    document.getElementById("navBar").style.top = "-5.25rem";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+window.onload = changeStyleColour;
